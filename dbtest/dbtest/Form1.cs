b22241d9620dc0dccdac230e8043b6dcd3bf3e73
Adeel Khan <Adeel Khan@DESKTOP-E3NIMMB>
2022-03-28 13:16:08 +0500

    changes

--- dbtest/dbtest/Form1.cs
+++ dbtest/dbtest/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,16 @@ namespace dbtest
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True");
+            if(con.State == ConnectionState.Closed)
+            {
+                con.Open();
+                MessageBox.Show("Connection established");
+            }
         }
     }
 }
